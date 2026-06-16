55c73180a7e3ddf0df25a5af7f3477253a3669fd
sparrowsurya <92842745+SparrowSurya@users.noreply.github.com>
2026-06-16 14:24:17 +0530

    Renamed scene file name.

--- way/scene/__init__.py
+++ way/scene/__init__.py
@@ -6,8 +6,8 @@ This submodule contains scene related classes and functions.
 from .constants import Scene
 from .protocols import GameScene
 from .main_menu import MainMenuScene
-from .maze_play import GamePlayScene
-from .end import GameEndScene
+from .game_play import GamePlayScene
+from .game_end import GameEndScene
 
 
 __all__ = (
